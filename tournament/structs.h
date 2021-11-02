366c5d650246a676d625494777b069f60abced6f
GLOBOSTOFO <71722881+GLOBOSTOFO@users.noreply.github.com>
2021-11-02 18:14:59 +0100

    Update structs.h

--- tournament/structs.h
+++ tournament/structs.h
@@ -5,41 +5,28 @@ using namespace std;
 
 struct player {
     /// Structure de base d'un participant du tournoi
-    unsigned id;
     string first_name;
     string last_name;
 
     friend ostream & operator<<(ostream & os, const player & p) {
-        os << "["
-           << p.id
-           << "] "
-           << p.first_name
+        os << p.first_name
            << " "
            << p.last_name;
         return os;
     }
 
     friend bool operator<(const player & lhs, const player & rhs) {
-        return lhs.id < rhs.id;
+        return lhs.last_name < rhs.last_name || (lhs.last_name == rhs.last_name && lhs.first_name < rhs.first_name);
     }
 };
 
 struct team {
     /// Strucutre de base d'une equipe
-    unsigned id;
     string name;
     unsigned points=0;
     vector<player> players;
 
-    friend ostream& operator<<(ostream& os, const team & t) {
-        os << "[" << t.id << "] " << t.name << " (" << t.points << " pts) :";
-        for (unsigned i=0; i<t.players.size(); i++) {
-            os << "\n  - " << t.players[i];
-        }
-        return os;
-    }
-
     friend bool operator<(const team & lhs, const team & rhs) {
-        return lhs.points > rhs.points || (lhs.points == rhs.points && lhs.id < rhs.id);
+        return lhs.points > rhs.points || (lhs.points == rhs.points && lhs.name < rhs.name);
     }
 };
