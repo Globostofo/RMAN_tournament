b78f0a15f778630d4f4b6401f489cdb45d29da0d
GLOBOSTOFO <71722881+GLOBOSTOFO@users.noreply.github.com>
2021-11-02 18:12:33 +0100

    Add files via upload

---
+++ global.hpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+namespace global_const {
+    const string NAMESPATH = "../S101/tournament/students.txt";
+
+    const string waitMsg = "Appuyez sur <ENTREE> pour continuer";
+
+    const unsigned MINTEAMS = 2;
+    const unsigned MAXTEAMS = 15;
+
+    const vector<string> GAMESNAME = {"memory",
+                                      "morpion",
+                                      "puissance 4",
+                                      "dames",
+                                      "echecs"};
+    const unsigned NBGAMES = GAMESNAME.size();
+}
+
+namespace global_func {
+
+    void test() {
+        cout << "test" << endl;
+    }
+
+    vector<string> readFile(const string & path) {
+        /// Fonction utilisée pour obtenir la liste des lignes d'un fichier de texte
+        vector<string> allLines;
+        ifstream ifs (path);
+        if (!ifs) return allLines;
+        for (string oneLine; getline(ifs, oneLine);)
+            allLines.push_back(oneLine);
+        return allLines;
+    }
+
+    string ask4UInput(const string & invite) {
+        /// Fonction utilisee pour demander une entree a l'utilisateur
+        cout << invite;
+        string answer;
+        getline(cin, answer);
+        return answer;
+    }
+
+    void pressEnter() {
+        ask4UInput(global_const::waitMsg + "\n");
+    }
+
+    template <typename T>
+    vector<T> randomSort(const vector<T> & v) {
+        // Algorithme de permutations aléatoire dans une liste
+        vector<T> vSorted;
+        for (unsigned i=0; i<v.size(); i++)
+            vSorted.push_back(v[i]);
+        T temp;
+        unsigned index;
+        for (unsigned i=0; i<vSorted.size(); i++) {
+            index = rand() % vSorted.size();
+            temp = vSorted[index];
+            vSorted[index] = vSorted[i];
+            vSorted[i] = temp;
+        }
+        return vSorted;
+    }
+
+    string completion0X(const string & txt, const unsigned nb_char) {
+        string output = txt;
+        while (output.size() < nb_char) {
+            output = "0" + output;
+        }
+        return output;
+    }
+}
