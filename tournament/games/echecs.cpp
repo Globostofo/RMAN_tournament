2f11f2e6048a73ba5dd4b16eb18013c2a9b02ec6
GLOBOSTOFO <71722881+GLOBOSTOFO@users.noreply.github.com>
2021-11-10 14:33:25 +0100

    Add files via upload

--- tournament/games/echecs.cpp
+++ tournament/games/echecs.cpp
@@ -5,16 +5,20 @@
 using namespace std;
 using namespace global_func;
 
-struct piece {
-    char type;
-    char color;
+struct pos {
     char column;
     unsigned line;
 };
 
+struct piece {
+    char type;
+    char color;
+    pos position;
+};
+
 char getPieceTypeByCoords(const char & c, const unsigned & l, const vector<piece> & pieces) {
     for(const piece & p : pieces) {
-        if (p.column == c && p.line == l)
+        if (p.position.column == c && p.position.line == l)
             return p.type;
     }
     return ' ';
@@ -44,16 +48,16 @@ vector<piece> startPieces() {
     vector<char> colorsChar = {'w', 'b'};
     unsigned piL=1, paL=2;
     for (unsigned i=0; i<2; i++) {
-        pieces.push_back(piece {'T', colorsChar[i], 'a', piL});
-        pieces.push_back(piece {'C', colorsChar[i], 'b', piL});
-        pieces.push_back(piece {'F', colorsChar[i], 'c', piL});
-        pieces.push_back(piece {'D', colorsChar[i], 'd', piL});
-        pieces.push_back(piece {'R', colorsChar[i], 'e', piL});
-        pieces.push_back(piece {'F', colorsChar[i], 'f', piL});
-        pieces.push_back(piece {'C', colorsChar[i], 'g', piL});
-        pieces.push_back(piece {'T', colorsChar[i], 'h', piL});
+        pieces.push_back(piece {'T', colorsChar[i], pos {'a', piL}});
+        pieces.push_back(piece {'C', colorsChar[i], pos {'b', piL}});
+        pieces.push_back(piece {'F', colorsChar[i], pos {'c', piL}});
+        pieces.push_back(piece {'D', colorsChar[i], pos {'d', piL}});
+        pieces.push_back(piece {'R', colorsChar[i], pos {'e', piL}});
+        pieces.push_back(piece {'F', colorsChar[i], pos {'f', piL}});
+        pieces.push_back(piece {'C', colorsChar[i], pos {'g', piL}});
+        pieces.push_back(piece {'T', colorsChar[i], pos {'h', piL}});
         for (char j='a'; j<='h'; j++) {
-            pieces.push_back(piece {'P', colorsChar[i], j, paL});
+            pieces.push_back(piece {'P', colorsChar[i], pos {j, paL}});
         }
         piL=8, paL=7;
     }
@@ -65,19 +69,14 @@ unsigned echecs(const string & t1, const string & t2) {
 
     vector<string> teams = {t1, t2};
     unsigned turn = rand()%2;
-    cout << "L'equipe " << teams[turn] << " joue les blancs" << endl
-         << "L'equipe " << teams[(turn+1)%2] << " joue les noirs" << endl;
+    cout << "L'equipe " << teams[turn] << " joue avec les blancs." << endl
+         << "L'equipe " << teams[(turn+1)%2] << " joue avec les noirs." << endl;
     pressEnter();
-    
+
     // Creation des pieces
     vector<piece> pieces = startPieces();
-    
+
     dispChessboard(pieces);
 
     return turn;
-}
-
-int main() {
-    echecs("equpe1", "equipe2");
-    return 1;
 }
